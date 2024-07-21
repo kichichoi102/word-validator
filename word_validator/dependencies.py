3a8f7cc49f6102d63becefef6b7d1ad44ac67aef
kichichoi102 <kichichoi102@gmail.com>
2024-07-21 13:13:17 -0400

    WV-012

--- word_validator/dependencies.py
+++ word_validator/dependencies.py
@@ -1,12 +1,9 @@
 from functools import lru_cache
-import logging
 
 from fastapi import Request
 
 from word_validator.core.trie import Trie
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 @lru_cache(maxsize=1)
 def initialize_trie(file_path: str) -> Trie:
@@ -14,6 +11,5 @@ def initialize_trie(file_path: str) -> Trie:
 
 
 def get_trie_from_app(request: Request) -> Trie:
-    logging.info(request.app.state.config.dictionary_file_path)
     trie: Trie = request.app.state.trie
     return trie
